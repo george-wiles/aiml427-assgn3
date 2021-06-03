3214714a7db9bbedcc44c4b3ddd08e3cf5447c17
George Wiles <george@wiles.org.nz>
2021-06-03 21:51:20 +1200

    changes to vector transform

--- src/main/java/SparkKDDLoadTest.java
+++ src/main/java/SparkKDDLoadTest.java
@@ -1,12 +1,14 @@
 import org.apache.spark.ml.classification.LogisticRegression;
 import org.apache.spark.ml.classification.LogisticRegressionModel;
-import org.apache.spark.ml.feature.StringIndexer;
-import org.apache.spark.ml.feature.StringIndexerModel;
-import org.apache.spark.ml.feature.VectorIndexer;
-import org.apache.spark.ml.feature.VectorIndexerModel;
+import org.apache.spark.ml.feature.*;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.System.exit;
 
@@ -18,8 +20,6 @@ public class SparkKDDLoadTest {
 		if (args.length != 1) {
 			System.out.println("provide filename");
 			exit(0);
-		} else {
-			throw new RuntimeException("oooop[ppps");
 		}
 		String filename = args[0];
 
@@ -28,16 +28,21 @@ public class SparkKDDLoadTest {
 				//.master("local")
 				.getOrCreate();
 		Dataset<Row> df = spark.read().format("csv").load(filename);
+		String[] featureCols = {
+				"duration","protocol_type","service","flag","src_bytes","dst_bytes",
+				"land","wrong_fragment","urgent","hot","num_failed_logins","logged_in",
+				"num_compromised","root_shell","su_attempted","num_root","num_file_creations",
+				"num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login",
+				"count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate",
+				"same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count",
+				"dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate",
+				"dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate",
+				"dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate"
+		};
+		List cols = new LinkedList(Arrays.asList(featureCols));
+		cols.add("label");
 
-		Dataset<Row> ds = df.toDF("duration","protocol_type","service","flag","src_bytes","dst_bytes",
-						"land","wrong_fragment","urgent","hot","num_failed_logins","logged_in",
-						"num_compromised","root_shell","su_attempted","num_root","num_file_creations",
-						"num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login",
-						"count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate",
-						"same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count",
-						"dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate",
-						"dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate",
-						"dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate","label");
+		Dataset<Row> ds = df.toDF((String[])cols.toArray(new String[cols.size()]));
 
 		// Index labels, adding metadata to the label column.
 		// Fit on whole dataset to include all labels in index.
@@ -46,17 +51,16 @@ public class SparkKDDLoadTest {
 				.setOutputCol("indexedLabel")
 				.fit(ds);
 
-		// Automatically identify categorical features, and index them.
-		VectorIndexerModel featureIndexer = new VectorIndexer()
-				.setInputCol("features")
-				.setOutputCol("indexedFeatures")
-				.setMaxCategories(4)
-				.fit(ds);
+		System.out.println(labelIndexer.labels());
 
-		System.out.println("columns --> " + ds.columns());
+		VectorAssembler vectorAssembler = new VectorAssembler()
+				.setInputCols(featureCols)
+				.setOutputCol("features");
+		Dataset<Row> transformDs = vectorAssembler.transform(ds);
+
 
 		//Create training and test set
-		Dataset<Row>[] splits = ds.randomSplit (new double[]{0.7,0.3},123);
+		Dataset<Row>[] splits = transformDs.randomSplit (new double[]{0.7,0.3},123);
 		Dataset<Row> training = splits[0];
 		Dataset<Row> test = splits[1];
 
@@ -64,6 +68,8 @@ public class SparkKDDLoadTest {
 		LogisticRegression lr = new LogisticRegression()
 				.setMaxIter(10) //Set maximum iterations
 				.setRegParam(0.3) //Set Lambda
+				.setFeaturesCol("features")
+				.setLabelCol("indexedLabel")
 				.setElasticNetParam(0.8); //Set Alpha
 
 		// Fit the model
