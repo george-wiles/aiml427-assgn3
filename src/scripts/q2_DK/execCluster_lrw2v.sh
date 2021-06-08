4fe808d92c2772d5be3f07b54407e994423d31e7
David Knox <knoxdavi@wonderland.ecs.vuw.ac.nz>
2021-06-08 15:09:23 +1200

    more logs and trials for q2_DK

--- src/scripts/q2_DK/execCluster_lrw2v.sh
+++ src/scripts/q2_DK/execCluster_lrw2v.sh
@@ -6,10 +6,10 @@ do
         echo "Submitting bdce1 logistic regression to cluster with random seed $randomSeed"
         sleep 3
 
-	"args: input file, use scaling, use pca, pcaK, random seed vector size"
-	spark-submit --class q2.BigDataClusterEater5 --master local bdce1_lr5.jar q2input2/smallset.csv 0 0 1 123 20
-
 	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 0 1 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 1 1 20 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 20 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 1 0 1 $randomSeed 100
 
         echo "Completed kdd logistic regression on cluster with random seed $randomSeed"
 done
---
+++ src/scripts/q2_DK/execCluster_lrw2v2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#for randomSeed in 1 2 3 4 5
+for randomSeed in 123
+do
+        echo "Submitting bdce1 logistic regression to cluster with random seed $randomSeed"
+        sleep 3
+
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 0 1 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 5 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 10 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 15 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 20 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 25 $randomSeed 100
+	spark-submit --class q2.BigDataClusterEater5 --master yarn --deploy-mode cluster bdce1_lr5.jar q2input2/IMDB_Dataset_fixed2.csv 0 1 50 $randomSeed 100
+
+        echo "Completed kdd logistic regression on cluster with random seed $randomSeed"
+done
+echo "done."
+
